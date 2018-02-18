199aa367535c37a83b0093de34eef068bd24147d
Murtaza Yaqoob <murtaza.yaqoob>
2018-02-18 18:33:07 -0500

    Added basic project structure with no database support...yet.

---
+++ mangainfo/src/test/java/io/github/murtazay/mangainfo/MangainfoApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.murtazay.mangainfo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MangainfoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
